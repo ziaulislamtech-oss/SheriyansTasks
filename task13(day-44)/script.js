b34c213463441f880f01ad2ff9ae14eb7d2d8776
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2025-11-20 14:32:37 +0500

    littleUpdationRecucedTheArray

--- task13(day-44)/script.js
+++ task13(day-44)/script.js
@@ -1,4 +1,4 @@
-let arr = ['hellow i am Zia-Ul-Islam','I am a frontend developer', 'sheryian coding school is the best'];
+let arr = ['hellow i am Zia-Ul-Islam','I am a frontend developer', 'sheryians is the bestd'];
 let imgPath = ['./img/1.png','./img/2.png','./img/3.png','./img/4.png','./img/5.png','./img/6.png','./img/7.png','./img/8.png','./img/9.png','./img/10.png','./img/11.png'];
 let main = document.querySelector('main');
 let button = document.querySelector('button');
