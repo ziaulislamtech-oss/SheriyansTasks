19fbd6c1754c70bab783dbeb381b046956a198a7
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2025-12-01 22:38:22 +0500

    spotLightEffectCreated

--- task20(day52)/script.js
+++ task20(day52)/script.js
@@ -1,15 +1,6 @@
+let body = document.querySelector('body')
 let box = document.querySelector('.box')
-addEventListener('mousemove',(dets)=>{
-    console.log(dets.clientX,dets.clientY)
-    box.style.background= `radialGradien( ${dets.clientX} at${dets.clientX}px ${dets.clientY}px)`
-    // document.body.style.setProperty('--x',dets.clientX + 'px')
-    // document.body.style.setProperty('--y',dets.clientY + 'px')
-     box.style.background = `
-        radial-gradient(${dets.clientX}px at ${dets.clientX}px ${dets.clientY}px, red, blue)
-    `;
+body.addEventListener('mousemove',(dets)=>{
+    box.style.setProperty('--x', `${dets.clientX}px`);
+    box.style.setProperty('--y', `${dets.clientY}px`)
 })
-// box.addEventListener('mousemove', (dets) => {
-//     box.style.background = `
-//         radial-gradient(circle at ${dets.clientX}px ${dets.clientY}px, red, blue)
-//     `;
-// });
