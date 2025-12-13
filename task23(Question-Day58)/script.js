d995ce385794ef6209873bfc5a724f5089486262
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2025-12-13 07:20:07 +0500

    day58QuestionsSolved

---
+++ task23(Question-Day58)/script.js
@@ -0,0 +1,136 @@
+//        -------------------------Questions----------------------
+
+            
+//  --------------Quesation NO 1:-----------------
+        
+        // let user={
+//     Name : 'Zia-Khan',
+//     Email : 'ziaulislam.tech@gmail.com',
+//     logedIn : function(){
+    //        return console.log('user logged In')
+    //     }
+
+    // }
+// console.log(user.logedIn())
+
+// class User{
+    //     constructor(name,email){
+        //         this.name = name;
+        //         this.email = email
+        //     }
+        //     loggedIn(){
+            //         console.log('loged In');
+            //     }
+            // }
+            // let user1 = new User('mubashir','mubashir123@gmail.com');
+            // let user2 = new User('shouaib','shouaib123@gmail.com');
+            // let user3 = new User('abdusalam','abdussalam@gmail.com');
+            
+            
+            //  ------------------------------------------------------------
+            
+//  -----------Question No 2 : --------------------;
+
+            // let product = {
+                //     Name: 'shoes',
+                //     Price : 2000,
+                //     discountPrice: function(){
+                    //         this.Price = this.Price-500;
+                    //         return this.Price
+                    //     }
+                    // }
+                    // console.log(product.discountPrice())
+                    
+                    
+// --------------------------------------------------------------------------------------
+
+
+                    
+                    
+//  ----------------Question No 3 --------------------------------
+
+// class Car{
+    //     constructor(brand,speed){
+        //         this.brand = brand;
+        //         this.speed = speed;
+//     }
+//     drive(){
+//         return `${this.brand} - ${this.speed}`
+//     }
+// }
+// let car1 = new Car('honda',222)
+// console.log(car1.drive())
+
+//  ---------------------------------
+
+
+//  ----------------Question No 4--------------------------------
+
+// class Student{
+    //     constructor(name,rollNumber){
+        //         this.name = name;
+//         this.rollNumber = rollNumber;
+//     }
+//     introduce(){
+    //         return `hi ${this.name} your roll number is ${this.rollNumber}`;
+    //     }
+    // }
+    // // console.log()
+    // let stududent1 = new Student('zia-khan',12)
+// let stududent2 = new Student('awais-khan',22)
+// let stududent3 = new Student('bilal-khan',32)
+// console.log(stududent1.introduce())
+
+// -----------------------------------
+
+
+//  ----------------Question No 5 --------------------------------
+
+// let obj={
+    //     method1:function(){
+        //        console.log(this)
+        //     },
+        //     method2:()=>{
+            //         console.log(this)
+            //     }
+            // }
+            
+            // // console.log(obj.method1())
+            // obj.method1()
+// obj.method2()
+// console.log(obj.method2())
+
+
+//  ----------------Question No 6 --------------------------------
+
+// function User(){
+//     this.name = 'harsh-patel';
+// }
+// User.prototype.logedIN = function(){
+//     console.log('logedIN')
+// }
+// let an1 = new User()
+// console.log(an1)
+
+// -------------------------------------
+
+
+//  ---------------Question No 7 --------
+
+// function print(){
+//     console.log(this.name)
+// }
+// let obj = {
+//     name : 'Zia',
+
+// };
+// print.call(obj)
+// print()
+// function print(a,b,c,d){
+//     console.log(this.name)
+// }
+// let obj={
+//     name:'harsh'
+// }
+// let Printed=print.bind(obj)
+// Printed()
