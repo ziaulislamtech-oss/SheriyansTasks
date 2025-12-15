8e1e1a58453459b04ec415a3426fa8525b66e17a
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2025-12-15 22:58:49 +0500

    day59completed

--- Day59(AdavnceJavaScript)/script.js
+++ Day59(AdavnceJavaScript)/script.js
@@ -1,6 +1,26 @@
-function abcd(fn){
-    fn()
+//   ---------------------- CallBack Hell------------------
+// function abcd(fn){
+//     fn(function(fn3){
+//         fn3(function(fn5){
+//             fn5(function(){
+//                 console.log('i am fn6')
+//             })
+//         })
+//     })
+// }
+
+// abcd(function(fn2){
+//     fn2(function(fn4){
+//         fn4(function(fn6){
+//             fn6()
+//         })
+//     })
+// })
+// function(address,cb){}
+function iceCreamLaao(address,cb){
+    // logic
+    cb('icecream')
 }
-abcd(function(){
-    console.log('hellow call back')
+iceCreamLaao('gufranDukan',function(ice){
+    console.log(ice)
 })
