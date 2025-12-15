556e97e0362d2a0c509d98492b296662a517236b
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2025-12-15 23:12:42 +0500

    Day59QuestonOneDone

---
+++ Day60(solvingQuestion)/script.js
@@ -0,0 +1,15 @@
+// ------------Sovling Quesiton------------------
+
+// -------------Question No : 1-----------------
+
+// function profileLaoo(time,cb){
+//     console.log(`this code will run after ${time}sec...`)
+//     setTimeout(()=>{
+//         cb()
+//     },time)
+// }
+// profileLaoo(3000,function(){
+//     console.log('helllloooo')
+// })
+
+// ------------------------
