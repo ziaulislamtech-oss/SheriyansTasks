2bfd34af091e09cd90133cd53e1c4d84deccde49
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2025-12-16 23:26:34 +0500

    QuestionNo7completed

--- Day60(solvingQuestion)/20Questions.js
+++ Day60(solvingQuestion)/20Questions.js
@@ -1,12 +1,109 @@
+// ~~~~~~~~~~~~~~~~~~~~LEVEL-ONE ----------------
 // ----------------Question No:1------------
 
-function sayHello(cb){
-    console.log('call Back jaraha hain....')
-   setTimeout(()=>{
-     cb(`Hello Call Back`)
-   },2000)
-}
-sayHello(function(callBack){
-    console.log(callBack)
+// function sayHello(cb){
+//     console.log('call Back jaraha hain....')
+//    setTimeout(()=>{
+    //      cb(`Hello Call Back`)
+//    },2000)
+// }
+// sayHello(function(callBack){
+    //     console.log(callBack)
+    
+    // })
+    
+    // ------------------------------------
+    
+    
+    // ----------------Question No:2------------
+    
+    // function doubleKaro(number,cb){
+        //     console.log(`Number double horaha hain...`)
+        //     setTimeout(()=>{
+            //         cb(number+number)
+            //     },2000)
+            // }
+            // doubleKaro(10,function(dbNum){
+                //     console.log(dbNum)
+                // })
+                
+                // ------------------------------------
+                
+                
+                // ----------------Question No:3------------
+                
+                // function sayWelcome(name,cb){
+                    //     console.log(`welcome aaraha hain...`)
+//     setTimeout(()=>{
+    //         cb(`Welcome ${name}`);
+//     },1000)
+// }
+// sayWelcome('zikhan',function(welcome){
+    //     console.log(welcome)
+    // })
+    
+    // --------------Question No : 4------------
+    
+    // function sumDedo(num1,num2,cb){
+//     console.log('sum horaha hain...')
+//     setTimeout(()=>{
+//         cb(num1+num2);
+//     },2000)
+// }
 
+// sumDedo(2,5,function(Num){
+    //     console.log(Num)
+// })
+
+// ------------------------------------
+
+// -------------Question No: 5--------------
+
+// function arraykiLengthDo(arr,cb){
+//     console.log('array ki length arahe hain...')
+//     setTimeout(()=>{
+//         cb(arr.length)
+//     },2000)
+// }
+// arraykiLengthDo([1,2,3,4,5,6],function(length){
+//     console.log(length)
+// })
+
+
+// ------------------------------------
+
+// ~~~~~~~~~~~~~~~~~~~~LEVEL-TWO ----------------
+
+// -------------Question No: 6--------------
+
+
+// function userNameDo(userObj,cb){
+//     console.log(`user name araha hain....`)
+//     setTimeout(()=>{
+//         cb(userObj.name)
+//     },2000)
+// }
+
+// userNameDo({id:22,name:'muhammad khan'},function(username){
+//     console.log(username)
+// })
+
+// ----------------------------------------
+
+// -------------Question No: 7--------------
+
+function evenYaOddBatao(number,cb){
+    console.log(`number check horaha hian....!`)
+    setTimeout(()=>{
+        cb(number)
+    },1000)
+}
+evenYaOddBatao(9,function(num){
+    if(num%2===0) console.log(`even`)
+        else return console.log('odd')
 })
+
+
+// ----------------------------------------
+
+// -------------Question No: 8--------------
