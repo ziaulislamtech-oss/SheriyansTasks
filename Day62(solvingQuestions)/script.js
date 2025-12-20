cc1882ae31e96d12d94d2445746e56dd5a1d11c2
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2025-12-20 11:44:39 +0500

    question2SolvedOnlyFor1emailNotwholeArray

--- Day62(solvingQuestions)/script.js
+++ Day62(solvingQuestions)/script.js
@@ -1,33 +1,62 @@
 // --------Question No :1;
 // let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
-async function getWeather(city){
-    try{
-        let apiKey = `57a412c18e3fa9889a1cc9cba63e40ea`;
-       let raw = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-    );
+// async function getWeather(city){
+//     try{
+//         let apiKey = `57a412c18e3fa9889a1cc9cba63e40ea`;
+//        let raw = await fetch(
+//         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+//     );
 
-       if(!raw.ok){
-        throw new Error('city not found or something went wrong')
-       }
+//        if(!raw.ok){
+//         throw new Error('city not found or something went wrong')
+//        }
 
-       let realData = await raw.json()
-      if(realData.main.temp<0){
-        console.warn(`too cold out there ${realData.main.temp}`)
-        console.log(realData)
-    }
-    else if(realData.main.temp>40){
-        console.warn(`Too hot out there ${realData.main.temp}`)
-        console.log(realData)
-      }
-      else{
-        console.log(`tempreatur is ${realData.main.temp}`)
-       console.log(realData)
-      }
-    }
-    catch(error){
-        console.log(error)
-    }
+//        let realData = await raw.json()
+//       if(realData.main.temp<0){
+//         console.warn(`too cold out there ${realData.main.temp}`)
+//         console.log(realData)
+//     }
+//     else if(realData.main.temp>40){
+//         console.warn(`Too hot out there ${realData.main.temp}`)
+//         console.log(realData)
+//       }
+//       else{
+//         console.log(`tempreatur is ${realData.main.temp}`)
+//        console.log(realData)
+//       }
+//     }
+//     catch(error){
+//         console.log(error)
+//     }
+// }
+// getWeather('leh')
+
+//  ---------------------------
+
+// ----------------Question No : 2--------
+
+const users=[
+    'akahrsh@femal.com',
+    'ziakhan@gmail.com',
+    'mubashir@gmail.com'
+]
+function sendEmail(email){
+    return new Promise((resolve,reject)=>{
+        let time = Math.floor(Math.random()*5)
+        setTimeout(()=>{
+            let probability = Math.floor(Math.random()*10)
+            if(probability<=5) resolve('Email sent successfully')
+            else reject('email not sent...')
+        // console.log(probability)
+        },time*1000)
+    })
 }
-getWeather('leh')
+
+sendEmail('harsh@gmail.com')
+.then(function(data){
+    console.log(data)
+})
+.catch(function(error){
+    console.log(error)
+})
